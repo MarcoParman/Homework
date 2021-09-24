8a3dbd32ad29c979597258d39c13c0cf547b3240
Marco <90029335+MarcoParman@users.noreply.github.com>
2021-09-24 12:19:51 +0300

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,14 @@
+function checkTemp(temp) {
+    console.log("Temp on "+temp)
+
+if ( temp >= 21 && temp <= 40 ) {
+    return 0;
+
+} if ( temp > 0 && temp <= 20 ) {
+    return -1;
+
+} else if ( temp > 40 && temp < 60) 
+    return 1;
+
+
+}
